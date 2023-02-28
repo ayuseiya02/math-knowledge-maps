cc7d3402344cf02ecf80e18d6c4df5f82a964214
ayuseiya02 <ayuseiya02@gmail.com>
2023-02-27 18:13:20 -0600

    prep

--- mathml_trees/test_features.py
+++ mathml_trees/test_features.py
@@ -5,9 +5,13 @@ from StandardizeOpTree import *
 from ExtractFeatures import *
 
 trees = getTreesFromFile("tests/sample/quant-ph0001009.html")
+#trees = getTreesFromFile("tests/custom/basic3.xml")
 
 for tree in trees:
+    plotTree(tree)
+
     s_tree = standardizeOpTree(tree)
+    plotTree(s_tree)
 
     f = extractFeatures(s_tree)
     printFeatures(f)
